c2a52f1b252e0457c8d08d2d288431c71a71bf30
Michael Siebenmann <msiebenmann@student.ethz.ch>
2024-10-14 22:24:10 +0200

    restyle buttons and convert texts to TMP

---
+++ OpenMaskXR/Assets/Scripts/UI/UIController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField] private GameObject homeMenuParent;
+    [SerializeField] private GameObject queryMenuParent;
+    [SerializeField] private float fadeDuration = 0.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void ToggleQueryMenuVisibility(bool isVisible)
+    {
+        if (isVisible)
+            queryMenuParent.SetActive(true);
+        CanvasGroup[] canvasGroups = queryMenuParent.GetComponentsInChildren<CanvasGroup>();
+        StartCoroutine(FadeMenu(queryMenuParent, canvasGroups, isVisible));
+    }
+
+    public void ToggleHomeMenuVisibility(bool isVisible)
+    {
+        if (isVisible)
+            homeMenuParent.SetActive(true);
+
+        CanvasGroup[] canvasGroups = homeMenuParent.GetComponentsInChildren<CanvasGroup>();
+        StartCoroutine(FadeMenu(homeMenuParent, canvasGroups, isVisible));
+    }
+
+    private IEnumerator FadeMenu(GameObject parent, CanvasGroup[] canvasGroups, bool isVisible)
+    {
+        float startAlpha = isVisible ? 0f : 1f;
+        float endAlpha = isVisible ? 1f : 0f;
+        float elapsedTime = 0f;
+
+        // Gradually fade alpha over time
+        while (elapsedTime < fadeDuration)
+        {
+            elapsedTime += Time.deltaTime;
+            float currentAlpha = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / fadeDuration);
+
+            foreach (CanvasGroup cg in canvasGroups)
+            {
+                cg.alpha = currentAlpha;
+            }
+
+            yield return null;
+        }
+
+        // Ensure the final alpha is set properly
+        foreach (CanvasGroup cg in canvasGroups)
+        {
+            cg.alpha = endAlpha;
+            cg.interactable = isVisible;
+            cg.blocksRaycasts = isVisible;
+        }
+
+        // Deactivate the menu if it is no longer visible
+        if (!isVisible)
+            parent.SetActive(false);
+    }
+}
